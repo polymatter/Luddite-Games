e9184fd177af5b6d0a26857f374c2b2b05c76b82
Jeffrey Lake <glisher_rock@hotmail.com>
2012-02-11 16:32:00 +0000

    correct FactoryGirl syntax

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,9 +7,9 @@
 # end
 
 Factory.define :product do |p|
-  p.name = "Test Product of super-awesomeness"
-  p.description = "This is a description **test markup emphasis** \n-test markup list item \n-interesting things\nand stuff"
-  p.image = "test_product_img.png"
-  p.size = :packet
-  p.price = 2.99
+  p.name          "Test Product of super-awesomeness"
+  p.description   "This is a description **test markup emphasis** \n-test markup list item \n-interesting things\nand stuff"
+  p.image         "test_product_img.png"
+  p.size          :packet
+  p.price         2.99
 end
