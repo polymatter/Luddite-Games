98814d7295798c6b965c218b342c553be44d00d2
Jeffrey Lake <glisher_rock@hotmail.com>
2012-03-16 00:10:33 +0000

    trying to fix deploy on heroku, bootstrap.css not precompiled

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.assets.precompile += %w( bootstrap.css )
+
 # Initialize the rails application
 Ludditegames::Application.initialize!
