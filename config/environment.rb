9cbc1e7bd3dfe3f6c8a778517898d47de69d6700
Jeffrey Lake <glisher_rock@hotmail.com>
2012-03-16 00:15:25 +0000

    trying fix deploy to heroku, hoping removing an include to bootstrap on application.css will fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.assets.precompile += %w( bootstrap.css )
-
 # Initialize the rails application
 Ludditegames::Application.initialize!
